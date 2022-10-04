3f4399d7ad84b7022d21b36f8d27b79853cea2d9
I_Ishchuk <yaroslavischuck@gmail.com>
2022-10-04 19:21:36 +0300

    first commit

--- Jenkins_pipeline_with_parameters.groovy
+++ Jenkins_pipeline_with_parameters.groovy
@@ -2,7 +2,7 @@ pipeline {
     agent 'any'
     parameters {
         string(name: "branch", defaultValue: "main", description: "which branch to use")
-        string(name: "url", defaultValue: "main", description: "which url to use")
+        string(name: "url", defaultValue: "url", description: "which url to use")
         choice(name: "test", choices:["all", "dir", "file"],  description: "Run test stage for test targets [all, dir, file]")
         choice(name: "test_params", choices:["all", "param1", "param2"],  description: "Run test stage for test targets [all, param1, param2]")
         booleanParam(name: "save_logs", description: "save logs", defaultValue: false)
@@ -12,12 +12,12 @@ pipeline {
         stage('Test Repo Setup') {
             steps {
                println("URL does not exist")
-//                 if (params.url == "url") {
-//                   println("URL exists")
-//                   } else {
-//                   println("URL does not exist")
-//                   }
-//                 }
+                if (params.url == "url") {
+                  println("URL exists")
+                  } else {
+                  println("URL does not exist")
+                  }
+
                 }
             }
 
