63852b1db2e8b633ff2960f0870caab8c7198b73
I_Ishchuk <yaroslavischuck@gmail.com>
2022-10-04 19:32:29 +0300

    first commit

--- Jenkins_pipeline_with_parameters.groovy
+++ Jenkins_pipeline_with_parameters.groovy
@@ -6,7 +6,6 @@ pipeline {
         choice(name: "test", choices:["all", "dir", "file"],  description: "Run test stage for test targets [all, dir, file]")
         choice(name: "test_params", choices:["all", "param1", "param2"],  description: "Run test stage for test targets [all, param1, param2]")
         booleanParam(name: "save_logs", description: "save logs", defaultValue: false)
-        choice(name: "test_target", choices:["all", 1, "site1", "site2", "locals"],  description: "Run test stage for test targets [all, site1, site2, locals]")
         }
     stages {
         stage('Test Repo Setup') {
