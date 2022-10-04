c36c80f3ca58197a49c378113a798748caadb0d0
I_Ishchuk <yaroslavischuck@gmail.com>
2022-10-04 19:09:44 +0300

    first commit

--- Jenkins_pipeline_with_parameters.groovy
+++ Jenkins_pipeline_with_parameters.groovy
@@ -24,37 +24,39 @@ pipeline {
         }
         stage('Report') {
             steps {
-                def report(${params.branch}){
-                    description = "Report from Branch: ${branch}"
-	                println(description)
-                     if (params.url != "") {
-                        print("Run report generation for ${url}")
-                    }
-                }
-
-                def log_collection(){
-                    description = "Save logs for a test run. (${params.branch};${params.url}"
-                    if (params.save_logs == true) {
-                        print(description)
-                    }
-                }
+                println("URL does not exist")
+//                 def report(${params.branch}){
+//                     description = "Report from Branch: ${branch}"
+// 	                println(description)
+//                      if (params.url != "") {
+//                         print("Run report generation for ${url}")
+//                     }
+//                 }
+//
+//                 def log_collection(){
+//                     description = "Save logs for a test run. (${params.branch};${params.url}"
+//                     if (params.save_logs == true) {
+//                         print(description)
+//                     }
+//                 }
             }
         stage('Log Collection') {
             steps {
-                def report(${params.branch}){
-                    description = "Report from Branch: ${branch}"
-                    println(description)
-                    if (params.url != "") {
-                        print("Run report generation for ${url}")
-                    }
-                }
-
-                def log_collection(){
-                    description = "Save logs for a test run. (${params.branch};${params.url}"
-                    if (params.save_logs == true) {
-                        print(description)
-                    }
-                }
+                println("URL does not exist")
+//                 def report(${params.branch}){
+//                     description = "Report from Branch: ${branch}"
+//                     println(description)
+//                     if (params.url != "") {
+//                         print("Run report generation for ${url}")
+//                     }
+//                 }
+//
+//                 def log_collection(){
+//                     description = "Save logs for a test run. (${params.branch};${params.url}"
+//                     if (params.save_logs == true) {
+//                         print(description)
+//                     }
+//                 }
             }
         }
     }
