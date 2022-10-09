9ce7a7e4788d861158401ae21e32838bc8368723
I_Ishchuk <yaroslavischuck@gmail.com>
2022-10-09 12:35:44 +0300

    commit

--- Jenkins_pipeline_with_parameters.groovy
+++ Jenkins_pipeline_with_parameters.groovy
@@ -37,9 +37,8 @@ pipeline {
                     }
                 }
 
-
-                description = "Save logs for a test run. (${params.branch};${params.url}"
                 script {
+                    description = "Save logs for a test run. (${params.branch};${params.url}"
                     if (params.save_logs == true) {
                         print(description)
                     }
