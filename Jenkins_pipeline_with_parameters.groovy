c7a6547f78a70966375caf74683c3737f0517a5a
I_Ishchuk <yaroslavischuck@gmail.com>
2022-10-09 12:26:07 +0300

    commit

--- Jenkins_pipeline_with_parameters.groovy
+++ Jenkins_pipeline_with_parameters.groovy
@@ -30,22 +30,22 @@ pipeline {
             steps {
                 println("init Report stage")
 
+                script {
+                    def report(branch){
+                        description = "Report from Branch: ${branch}"
+                        println(description)
+                        if (params.url != "") {
+                            print("Run report generation for ${url}")
+                        }
+                    }
 
-                def report(branch){
-                    description = "Report from Branch: ${branch}"
-                    println(description)
-                    if (params.url != "") {
-                        print("Run report generation for ${url}")
+                    def log_collection(){
+                        description = "Save logs for a test run. (${params.branch};${params.url}"
+                        if (params.save_logs == true) {
+                            print(description)
+                        }
                     }
                 }
-
-                def log_collection(){
-                    description = "Save logs for a test run. (${params.branch};${params.url}"
-                    if (params.save_logs == true) {
-                        print(description)
-                    }
-                }
-
 
 
                 }
